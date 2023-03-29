7ceb92f6f7e00a71521e057cacefda6bf049e185
unknown <rashidul537@gmail.com>
2023-03-29 13:37:00 +0600

    garbage

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ActiveTodos from "./components/ActiveTodos";
 import AddTodo from "./components/AddTodo";
 import Header from "./components/Header";
 import TodoList from "./components/TodoList";
+import { fetchTodos } from "./redux/todoSlice";
 // import { fetchTodos } from "./redux/todoSlice";
 
 function App() {
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const todos = useSelector((state) => state.todoReducer.todos);
 
   // useEffect(() => {
   //   dispatch(fetchTodos());
   // });
-  window.localStorage.setItem("idk", "i dont know");
 
-  const idk = window.localStorage.getItem("idk");
   return (
     <div className="flex items-center justify-center py-10 ">
       <div className="flex  flex-col  min-h-[30%] min-w-[50%] ">
         <Header />
-        <h2>{idk || "No local storage"}</h2>
         <AddTodo todos={todos} />
         <TodoList todos={todos} />
-
         <ActiveTodos />
       </div>
     </div>
