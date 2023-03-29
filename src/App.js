35a7cd55ceaa6195059ea0377b4d3efc6809f969
unknown <rashidul537@gmail.com>
2023-03-29 13:21:02 +0600

    garbage

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import ActiveTodos from "./components/ActiveTodos";
 import AddTodo from "./components/AddTodo";
@@ -12,12 +13,14 @@ function App() {
   // useEffect(() => {
   //   dispatch(fetchTodos());
   // });
+  window.localStorage.setItem("idk", "i dont know");
 
+  const idk = window.localStorage.getItem("idk");
   return (
     <div className="flex items-center justify-center py-10 ">
       <div className="flex  flex-col  min-h-[30%] min-w-[50%] ">
         <Header />
-
+        <h2>{idk || "No local storage"}</h2>
         <AddTodo todos={todos} />
         <TodoList todos={todos} />
 
